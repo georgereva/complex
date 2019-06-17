defc0ac8b51ce9aae4f0a05c68ef84950b833d4f
georgereva <joravartan@gmail.com>
2019-06-17 22:02:07 +0300

    add

--- Class Complex/Complex.h
+++ Class Complex/Complex.h
@@ -1,111 +1,131 @@
 
 #include <iostream>
 #include <math.h>
- 
+
 using namespace std;
- 
- 
-class Complex // класс комплексных чисел
+
+
+class Complex
 {
-    double re, im; // целая и мнимая части
- 
-    // создаем конструкторы   
-    public:
- 
-    Complex() {};
- 
-    Complex (double r)
-    {
-        re = r; 
-        im = 0;
-    }
- 
-    Complex (double r, double i)
-    {
-        re = r; 
-        im = i;
-    } 
- 
-    Complex (Complex &c) // конструктор копирования
-    {
-        re = c.re; 
-        im = c.im;
-    }
- 
-    ~Complex() {}
- 
-    float fabs() // Модуль комплексного числа
-    {
-        return sqrt(re * re - im * im);
-    }     
- 
-    Complex & operator = (Complex &c) // перегрузка оператора присваивания
-    {
-        re = c.re;
-        im = c.im;
- 
-        return (*this);
-    }
- 
-    Complex operator + (Complex &c) // перегрузка оператора сложения
-    {
-      Complex temp;
- 
-        temp.re = re + c.re;
-        temp.im = im + c.re;
- 
-        return temp;
-    }  
- 
-    Complex operator - (Complex &c) // перегрузка оператора вычитания
-    {
-        Complex temp;
- 
-        temp.re = re - c.re;
-        temp.im = im - c.re;
- 
-        return temp;
-    }
- 
-    Complex operator * (Complex &c) // перегрузка оператора умножения
-    {
-        Complex temp;
- 
-        temp.re = re*c.re;
-        temp.im = re*c.im;
- 
-        return temp;
-    }
- 
-    Complex operator / (Complex &c) // перегрузка оператора деления
-    {
-        Complex temp;
- 
-        double r = c.re * c.re + c.im * c.im;
-        temp.re = (re * c.re + im * c.im) / r;
-        temp.im = (im * c.re - re * c.im) / r;
- 
-        return temp;
-    }   
- 
-    friend ostream &operator<<(ostream &, const Complex &); // перегрузка оператора <<
-    friend istream &operator>>(istream &, const Complex &); // перегрузка оператора >>
- 
+private:
+	double re, im; // целая и мнимая части
+
+	// создаем конструкторы   
+public:
+
+	Complex() {};
+
+	Complex(double r)
+	{
+		re = r;
+		im = 0;
+	}
+
+	Complex(double r, double i)
+	{
+		re = r;
+		im = i;
+	}
+
+	Complex(Complex& c) // конструктор копирования
+	{
+		re = c.GetRe();
+		im = c.GetIm();
+	}
+
+	~Complex() {}
+
+	float fabs() // Модуль комплексного числа
+	{
+		return sqrt(re * re - im * im);
+	}
+
+	Complex& operator = (Complex & c) // перегрузка оператора присваивания
+	{
+		re = c.GetRe();
+		im = c.GetIm();
+
+		return (*this);
+	}
+
+	Complex operator + (Complex & c) // перегрузка оператора сложения
+	{
+		Complex temp;
+
+		temp.SetRe(re + c.GetRe());
+		temp.SetIm(im + GetRe());
+
+		return temp;
+	}
+
+	Complex operator - (Complex & c) // перегрузка оператора вычитания
+	{
+		Complex temp;
+
+		temp.SetRe(re - c.GetRe());
+		temp.SetIm(im - c.GetRe());
+
+		return temp;
+	}
+
+	Complex operator * (Complex & c) // перегрузка оператора умножения
+	{
+		Complex temp;
+
+		temp.SetRe(re * c.GetRe());
+		temp.SetIm(re * c.GetRe());
+
+		return temp;
+	}
+
+	Complex operator / (Complex & c) // перегрузка оператора деления
+	{
+		Complex temp;
+
+		double r = c.GetRe() * c.GetRe(); +GetIm() * GetIm();
+		temp.SetRe((re * c.GetRe() + im * c.GetIm()) / r);
+		temp.SetIm((im * c.GetRe() - re * c.GetIm()) / r);
+
+		return temp;
+	}
+
+	friend ostream & operator<<(ostream&, const Complex&); // перегрузка оператора <<
+	friend istream & operator>>(istream&, const Complex&); // перегрузка оператора >>
+
+	double GetRe()
+	{
+		return re;
+	};
+	double GetIm()
+	{
+		return im;
+	};
+	int SetRe(double newVal)
+	{
+		re = newVal;
+		return 1;
+	};
+	int SetIm(double newVal)
+	{
+		im = newVal;
+		return 1;
+	};
+
 };
- 
-ostream &operator<<(ostream &out, const Complex &c)
+
+ostream& operator<<(ostream & out, const Complex & c)
 {
-    out << c.re;
-    if( c.im > 0 )
-        out << "+";
-    out << c.im << "i";
- 
-    return out;
+	out << c.re;
+	if (c.im > 0)
+		out << "+";
+	out << c.im << "i";
+
+	return out;
 }
- 
-istream &operator>>(istream &in, const Complex &c)
+
+istream& operator>>(istream & in, const Complex & c)
 {
-    in >> c.re >> c.im;
- 
-    return in;
+	in >> c.re >> c.im;
+
+	return in;
 }
